dbc6436927699b5e852174092109aeaa3a42a4a4
Jonathan Summers-Muir <MildTomato@users.noreply.github.com>
2022-06-10 12:57:21 +0800

    Update Appearance.stories.tsx

--- packages/react/src/components/Auth/Appearance.stories.tsx
+++ packages/react/src/components/Auth/Appearance.stories.tsx
@@ -14,41 +14,6 @@ export default {
   component: Auth,
 }
 
-const myButtonStyles = css({
-  fontFamily: 'arial',
-  display: 'flex',
-  alignItems: 'center',
-  justifyContent: 'center',
-  gap: '8px',
-  fontSize: '16px',
-  padding: '8px 4px',
-  cursor: 'pointer',
-  borderWidth: '2px',
-  borderStyle: 'solid',
-  width: '100%',
-
-  transitionPproperty: 'background-color',
-  transitionTimingFunction: 'cubic-bezier(0.4, 0, 0.2, 1)',
-  transitionDuration: '100ms',
-
-  variants: {
-    color: {
-      default: {
-        backgroundColor: 'black',
-        color: 'white',
-      },
-      primary: {
-        backgroundColor: 'black',
-        color: 'white',
-        borderColor: 'black',
-        '&:hover': {
-          backgroundColor: 'darkgray',
-        },
-      },
-    },
-  },
-})
-
 const Container = (props: any) => {
   const { user } = Auth.useUser()
   if (user)
@@ -64,29 +29,6 @@ const Container = (props: any) => {
     <div style={{ maxWidth: '320px', margin: 'auto' }}>{props.children}</div>
   )
 }
-
-/**
- *
- * [1] global themes
- * [2] dark mode
- * [3] extend appearance
- * [4] prepend css class name
- * [5] extend css class
- * [6] add inline style
- * [7] extendAppearance bool
- * [8] dark mode
- * [9] localization
- * [10] localization variables
- *
- * future:
- *
- * [1] phone auth support, OTP form
- * [2] treeshake imports for individual views
- * [3] prop for callback error support "onError", "onResponse", "onChange".
- *      also support a 'hideMessages' prop
- * [4] more inputs, username etc. maybe custom inputs
- *
- */
 
 export const Default = (args: any) => {
   return (
@@ -107,9 +49,6 @@ export const Default = (args: any) => {
                 },
               },
             },
-          }}
-          localization={{
-            lang: 'ja',
           }}
         />
       </Container>
