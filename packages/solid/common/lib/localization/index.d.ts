4fa8af4a5d443953813ecea31463387b67c7f386
Michael Essiet <essiet2002@gmail.com>
2022-11-05 16:33:33 +0100

    removed tsc compiled files that I forgot to remove

--- packages/solid/common/lib/localization/index.d.ts
+++
@@ -1,3 +0,0 @@
-export { default as en } from './en.json';
-export { default as ja } from './ja.json';
-//# sourceMappingURL=index.d.ts.map
--- packages/solid/common/lib/localization/index.js
+++
@@ -1,2 +0,0 @@
-export { default as en } from './en.json';
-export { default as ja } from './ja.json';
