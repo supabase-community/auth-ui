75612fdda2d91a24106c7579296bf1d9eae9f730
Michael Essiet <essiet2002@gmail.com>
2022-12-25 20:23:53 +0100

    exported german localizations

--- packages/solid/common/lib/localization/index.ts
+++ packages/solid/common/lib/localization/index.ts
@@ -1,2 +1,4 @@
 export { default as en } from './en.json'
 export { default as ja } from './ja.json'
+export { default as de_formal } from './de_formal.json'
+export { default as de_informal } from './de_informal.json'
