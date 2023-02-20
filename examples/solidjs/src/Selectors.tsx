c4e05b9822e616a3ac9909cd884954d92d4111b1
Andrew Smith <andrew@supabase.io>
2023-02-20 01:32:42 +0000

    Move solidjs example to the examples directory (#123)

--- examples/solidjs/src/Selectors.tsx
+++ examples/solidjs/src/Selectors.tsx
@@ -1,12 +1,12 @@
-import ToggleButton from "@suid/material/ToggleButton";
-import ToggleButtonGroup from "@suid/material/ToggleButtonGroup";
-import Circle from "@suid/icons-material/Circle";
-import RoundedCorner from "@suid/icons-material/RoundedCorner";
-import RoundedCornerRounded from "@suid/icons-material/RoundedCornerRounded";
-import RoundedCornerSharp from "@suid/icons-material/RoundedCornerSharp";
-import HorizontalIcon from "@suid/icons-material/AlignHorizontalCenterRounded";
-import VerticalIcon from "@suid/icons-material/AlignVerticalCenterRounded";
-import styles from "./App.module.css";
+import ToggleButton from '@suid/material/ToggleButton'
+import ToggleButtonGroup from '@suid/material/ToggleButtonGroup'
+import Circle from '@suid/icons-material/Circle'
+import RoundedCorner from '@suid/icons-material/RoundedCorner'
+import RoundedCornerRounded from '@suid/icons-material/RoundedCornerRounded'
+import RoundedCornerSharp from '@suid/icons-material/RoundedCornerSharp'
+import HorizontalIcon from '@suid/icons-material/AlignHorizontalCenterRounded'
+import VerticalIcon from '@suid/icons-material/AlignVerticalCenterRounded'
+import styles from './App.module.css'
 import {
   customBorderRadius,
   customButtonColor,
@@ -16,14 +16,13 @@ import {
   setCustomButtonColor,
   setCustomSocialLayout,
   setCustomTheme,
-} from "./store";
-import Logo from "../logo.svg";
-import { MaterialUISwitch } from "./muiswitch";
-import { createEffect } from "solid-js";
+} from './store'
+import Logo from './logo.svg'
+import { MaterialUISwitch } from './muiswitch'
 
 function Selectors() {
   return (
-    <div class={styles["selectors-container"]}>
+    <div class={styles['selectors-container']}>
       <h2>Auth UI Solid.js</h2>
       <p>
         Customizable authentication UI component with custom themes and
@@ -32,44 +31,43 @@ function Selectors() {
       <div class={styles.solidrow}>
         <img src={Logo} alt="Solid.js Logo" height={32} />
         <p>
-          Currently available in Solid.js and{" "}
+          Currently available in Solid.js and{' '}
           <a href="https://supabase.com/docs/guides/auth/auth-helpers/auth-ui#predefined-themes">
             React
           </a>
         </p>
       </div>
-      <div class={styles["toggles-grid"]}>
+      <div class={styles['toggles-grid']}>
         <ColorToggles />
         <RadiusToggles />
         <AlignmentToggles />
       </div>
     </div>
-  );
+  )
 }
 
 function ThemeToggle() {
   function handleToggle() {
     setCustomTheme((prev) => {
-      return prev === "dark" ? "default" : "dark";
-    });
+      return prev === 'dark' ? 'default' : 'dark'
+    })
   }
-
 
   return (
     <div>
       <h5>UI Theme</h5>
       <MaterialUISwitch
-        checked={customTheme() === "dark" ? true : false}
+        checked={customTheme() === 'dark' ? true : false}
         onChange={handleToggle}
       />
     </div>
-  );
+  )
 }
 
 function AlignmentToggles() {
   function handleChange(e: MouseEvent, newAlignment: string) {
     if (newAlignment !== null) {
-      setCustomSocialLayout(newAlignment);
+      setCustomSocialLayout(newAlignment)
     }
   }
 
@@ -82,20 +80,20 @@ function AlignmentToggles() {
         exclusive
       >
         <ToggleButton value={socialAlignment.horizontal}>
-          <VerticalIcon style={{ color: "white" }} />
+          <VerticalIcon style={{ color: 'white' }} />
         </ToggleButton>
         <ToggleButton value={socialAlignment.vertical}>
-          <HorizontalIcon style={{ color: "white" }} />
+          <HorizontalIcon style={{ color: 'white' }} />
         </ToggleButton>
       </ToggleButtonGroup>
     </div>
-  );
+  )
 }
 
 function RadiusToggles() {
   function handleChange(e: MouseEvent, newRadius: string) {
     if (newRadius !== null) {
-      setCustomBorderRadius(newRadius);
+      setCustomBorderRadius(newRadius)
     }
   }
   return (
@@ -107,23 +105,23 @@ function RadiusToggles() {
         onChange={handleChange}
       >
         <ToggleButton value={radii.small}>
-          <RoundedCornerSharp style={{ color: "white" }} />
+          <RoundedCornerSharp style={{ color: 'white' }} />
         </ToggleButton>
         <ToggleButton value={radii.medium}>
-          <RoundedCorner style={{ color: "white" }} />
+          <RoundedCorner style={{ color: 'white' }} />
         </ToggleButton>
         <ToggleButton value={radii.large}>
-          <RoundedCornerRounded style={{ color: "white" }} />
+          <RoundedCornerRounded style={{ color: 'white' }} />
         </ToggleButton>
       </ToggleButtonGroup>
     </div>
-  );
+  )
 }
 
 function ColorToggles() {
   function handleChange(e: MouseEvent, newColor: string) {
     if (newColor !== null) {
-      setCustomButtonColor(newColor);
+      setCustomButtonColor(newColor)
     }
   }
 
@@ -149,30 +147,30 @@ function ColorToggles() {
         </ToggleButton>
       </ToggleButtonGroup>
     </div>
-  );
+  )
 }
 
-export default Selectors;
+export default Selectors
 
 const colors = {
-  red: "rgb(202, 37, 37)",
-  green: "rgb(65, 163, 35)",
-  blue: "rgb(8, 107, 177)",
-  orange: "rgb(235, 115, 29)",
-};
+  red: 'rgb(202, 37, 37)',
+  green: 'rgb(65, 163, 35)',
+  blue: 'rgb(8, 107, 177)',
+  orange: 'rgb(235, 115, 29)',
+}
 
 const radii = {
-  small: "5px",
-  medium: "10px",
-  large: "20px",
-};
+  small: '5px',
+  medium: '10px',
+  large: '20px',
+}
 
 const socialAlignment = {
-  horizontal: "horizontal",
-  vertical: "vertical",
-};
+  horizontal: 'horizontal',
+  vertical: 'vertical',
+}
 
 const theme = {
-  light: "light",
-  dark: "dark",
-};
+  light: 'light',
+  dark: 'dark',
+}
